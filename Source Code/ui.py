9162d8d35c682e7e5a63c3105b0e1938d2ab9108
Ben Schuman <ben@jeffschuman.com>
2019-12-12 15:40:33 -0500

    Added ability to run network directly from UI via "run network button" below dropdown menus

--- Source Code/ui.py
+++ Source Code/ui.py
@@ -1,9 +1,41 @@
 import tkinter as tk
 import xlrd
+from joblib import dump, load
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import numpy as np
+from tkinter import messagebox
 
 
-def run_network():
-    print("do something")
+def create_mapped_dict(filepath):
+    workbook = xlrd.open_workbook(filepath)
+    sheet = workbook.sheet_by_index(0)
+
+    mapped_dict = {}
+
+    for i in range(0, sheet.nrows):
+        key = sheet.cell(i, 0).value
+        val = int(sheet.cell(i, 1).value)
+
+        mapped_dict[key] = val
+
+    return mapped_dict
+
+
+def run_network(region_text, disaster_text):
+    svc_class = load('../Resources/model.joblib')
+    region_dict = create_mapped_dict('../Resources/indexedRegions.xlsx')
+    disaster_dict = create_mapped_dict('../Resources/indexedDisasters.xlsx')
+
+    region_idx = region_dict[region_text]
+    disaster_idx = disaster_dict[disaster_text]
+
+    inputs = np.asarray([[region_idx, disaster_idx]])
+
+    tk.messagebox.showinfo("Result", str(int(svc_class.predict(inputs)[0])) + ' People')
+
+
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -11,8 +43,6 @@ class Application(tk.Frame):
         self.master = master
         self.grid()
         self.create_widgets()
-
-
 
     def create_widgets(self):
         workbook = xlrd.open_workbook('../Resources/idmc_disaster_all_dataset.xlsx')
@@ -37,13 +67,19 @@ class Application(tk.Frame):
         category_label = tk.Label(self, text="Pick Natural Disaster Type:")
         category_label.grid(row=2, column=0)
 
-        btn = tk.Button(self.master, text="Run Network", command=run_network)
+        tkvar2 = tk.StringVar(self.master)
+        tkvar2.set("Flood")
+        category = tk.OptionMenu(self, tkvar2, 'Flood', 'Extreme Temperature', 'Earthquake', 'Wet Mass Movement',
+                                 'Storm', 'Dry Mass Movement', 'Drought', 'Volcanic Eruption', 'Wildfire',
+                                 'Mass Movement', 'Volcanic Activity', 'Severe Winter Condition')
+        category.grid(row=3, column=0)
+
+        btn = tk.Button(self.master, text="Run Network", command=lambda:run_network(tkvar.get(), tkvar2.get()))
         btn.grid(row=3, column=0)
 
-        tkvar = tk.StringVar(self.master)
-        tkvar.set("Flood")
-        category = tk.OptionMenu(self, tkvar, 'Flood', 'Extreme Temperature', 'Earthquake', 'Wet Mass Movement', 'Storm', 'Dry Mass Movement', 'Drought', 'Volcanic Eruption', 'Wildfire', 'Mass Movement', 'Volcanic Activity', 'Severe Winter Condition')
-        category.grid(row=3, column=0)
+
+
+
 
 
 #TODO: add neural network to UI
